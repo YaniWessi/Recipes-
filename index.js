52f613a8c0371ee2995bd8198a24b0a228587ea8
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2022-09-29 16:52:34 -0400

    wrote a post lessons end point

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
+
+let hubs = []
+
+let lessons = []
+
+server.use(express.json())
+
+server.get("/", (req,res) => {
+    res.status(200).json({api:"running..."})
+})
+
+server.get("/hello", (req,res) => {
+    res.status(200).json({hello: "web 27"})
+})
+
+server.post("/api/hubs", (req, res) => {
+    const hubInfo = req.body;
+
+    hubInfo.id = shortid.generate();
+    hubs.push(hubInfo)
+
+    res.status(201).json(hubInfo);
+})
+
+server.post("/api/lessons", (req,res) => {
+    const lesson = req.body;
+
+    lesson.id = shortid.generate();
+
+    lessons.push(lesson)
+
+    res.status(201).json(lesson);
+})
 
 const PORT = 5000;
 server.listen(PORT, () => console.log(`\n ** API on http://localhost:${PORT} **\n`));
